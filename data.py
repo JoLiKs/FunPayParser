d63baa92c017f96ca2344d5f79b5227e806ea928
Egor Ralovich <vonkad002@gmail.com>
2023-02-15 19:21:54 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,3 @@
-class Flat:
-    def __init__(self, link, reference=None, price=None, title=None, description=None, date=None):
-        self.link = link
-        self.reference = reference
-        self.price = price
-        self.title = title
-        self.description = description
-        self.date = date
-
-
 class Lot:
     def __init__(self, link, reference=None, price=None, title=None, seller_rep=None):
         self.link = link
